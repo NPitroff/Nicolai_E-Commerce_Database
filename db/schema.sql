03f87dfd7aa39007493d18e2aa773e8408ad0b89
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-14 09:13:22 -0800

    ADDED 'REQUIREMENTS' IN THE SERVER, WITH A SYNC TO SEQUELIZE

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,8 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+-- ===========USE ECOMMERCE_DB===========--
+USE ecommerce_db;
+-- ===========CREATE A TABLE TO HOLD "CATEGORY"============ --
+
 
