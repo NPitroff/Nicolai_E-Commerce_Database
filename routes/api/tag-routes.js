172413f89375d8239dbf0e0f8a4b17b9a484395b
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-14 14:53:33 -0800

    ADDED A 'DELETE' ROUTE FOR 'TAG'

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,16 +31,37 @@ router.get('/:id', (req, res) => {
 }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+//=======ADDED AN 'ASYNC' COMMAND===========//
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
