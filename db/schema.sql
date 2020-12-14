a1272841aa487c231fb0301ee59b3ea4ae798b8b
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-14 10:20:40 -0800

    ADDED A TABLE FOR THE 'PRODUCT' INFORMATION

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,19 @@ CREATE DATABASE ecommerce_db;
 -- ===========USE ECOMMERCE_DB===========--
 USE ecommerce_db;
 -- ===========CREATE A TABLE TO HOLD "CATEGORY"============ --
-
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT,
+    cat_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+--===========CREATE A TABLE TO HOLD THE PRODUCT==============--
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT,
+    pro_name VARCHAR(50),
+    price DECIMAL(10,4) NOT NULL,
+    stock INT NOT NULL DEFAULT ISNUMERIC(10),
+    FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE
+    PRIMARY KEY(id)
+);
+--===========
 
